26eb5f7326c2bbd7387ffc61bf2af2f5c1c816db
Александр Ильин <sasha8@list.ru>
2023-07-06 20:11:41 +0300

    Задание 1 выполнено

---
+++ Урок 4/Задание 1/Zadanie_1.java
@@ -0,0 +1,80 @@
+// Даны два Deque, представляющие два целых числа. Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+// 1) Умножьте два числа и верните произведение в виде связанного списка.
+// 2) Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными.
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Zadanie_1 {
+    public static void main(String[] args) {
+        clearScreen();
+
+        Deque<Integer> deque1 = new LinkedList <Integer>();
+        deque1.addFirst(1);
+        deque1.addFirst(2);
+        deque1.addFirst(3);
+       
+
+        Deque<Integer> deque2 = new LinkedList <Integer>();
+        deque2.addFirst(2);
+        deque2.addFirst(2);
+        deque2.addFirst(3);
+        deque2.addFirst(3);
+
+        Deque<Integer> dequeTemp = new LinkedList <Integer>();
+
+
+        System.out.println(deque1);
+        System.out.println(deque2);
+
+
+        dequeTemp = deque1;
+        int chislo1 = 0;
+        int i = 1;
+        while (true) {
+            var chislo = dequeTemp.pollFirst();
+            if(chislo == null) {break;}
+            else{
+            chislo1 += chislo * i;
+            i *= 10;}
+        }
+
+        dequeTemp = deque2;
+        int chislo2 = 0;
+        i = 1;
+        while (true) {
+            var chislo = dequeTemp.pollFirst();
+            if(chislo == null) {break;}
+            else{
+            chislo2 += chislo * i;
+            i *= 10;
+            }
+        }
+        System.out.println(chislo1);
+        System.out.println(chislo2);
+        
+
+        int chislo3 = chislo1 * chislo2;
+
+        System.out.print("* ");
+        System.out.println(chislo3);
+
+        Deque<Integer> deque3 = new LinkedList <Integer>();
+        i = 10;
+        while(true){
+            int chislo = chislo3 % i;
+            if(chislo == 0) break;
+            deque3.addLast(chislo);
+            chislo3 /= 10;
+        }
+
+        System.out.println(deque3);
+    }
+
+     // Очистка консоли:
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
